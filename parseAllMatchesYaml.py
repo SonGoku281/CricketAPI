4f2d8061f016d585a0a51fd7006cd9554e10169e
SonGoku281 <mohanty.swagat@protonmail.com>
2019-12-29 00:57:38 +0530

    parsing the yaml file- need to implement parallel processing to speed up the process

--- parseAllMatchesYaml.py
+++ parseAllMatchesYaml.py
@@ -1,1 +1,16 @@
-# since the cricksheet is downloaded in yaml format we need to 
+# since the cricksheet is downloaded in yaml format we need to parse each file and get the data into our database
+import os
+import yaml
+
+path = "./allMatches"
+count = 0 
+for filename in os.listdir(path):
+    if filename.endswith(".yaml"): 
+         # parse the yaml file and fill the database.
+         with open(path+"/"+filename) as f:
+            # use safe_load instead load
+            dataMap = yaml.safe_load(f) 
+            count = count + 1
+    f.close()
+    
+count = count +1
